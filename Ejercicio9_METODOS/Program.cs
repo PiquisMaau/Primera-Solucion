5e65cba3680ac83d1ccf5f25c5cfc23c0032c7aa
Mauro Pico <sebaspico@outlook.com>
2026-03-04 09:20:21 -0500

    Documentacion mejorada

--- Ejercicio9_METODOS/Program.cs
+++ Ejercicio9_METODOS/Program.cs
@@ -15,6 +15,14 @@
             {
                 Console.WriteLine(DevolverNumerosGanadores()[0]);
             }
+            Console.WriteLine("Imprimir Numeros ganadores con FOR-EACH");
+            foreach (int ganador in DevolverNumerosGanadores())
+            {
+                Console.WriteLine(ganador);
+            }
+            Console.WriteLine("------------------------------------------");
+            List<double> calificacionesEstudiantes = new List<double>() { 5.5, 6.9, 8.0, 9.5, 8.0 };
+            Console.WriteLine(ObtenerPromedioCalificaciones(calificacionesEstudiantes));
 
         }
 
@@ -42,6 +50,21 @@
                 int numeroAleatorio = random.Next(1, 26);
             }
             return listaNumerosGanadores;
+        }
+        /// <summary>
+        /// El metodo toma una lista de calificacione de tipo double, y calcula el promedio. 
+        /// </summary>
+        /// <param name="calificacionesEstudiantes"></param>
+        /// <returns>Retorna una calificación de tipo double</returns>
+        static double ObtenerPromedioCalificaciones(List<double> calificacionesEstudiantes)
+        {
+            double suma=0, promedio = 0;
+            foreach (double calificacion in calificacionesEstudiantes)
+            {
+                suma += calificacion;
+            }
+                promedio = suma / calificacionesEstudiantes.Count;
+            return promedio;
         }
     }
 }
